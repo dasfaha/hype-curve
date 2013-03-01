b5e62483b74aa77c66363c357b1dbbb94a0e2c21
David Asfaha <davidasfaha@gmail.com>
2013-03-01 14:11:26 +0000

    add database schema

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE "source" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "name" TEXT NOT NULL,
+    "url" TEXT NOT NULL
+);
+CREATE TABLE data (
+    "id" INTEGER PRIMARY KEY,
+    "title" TEXT,
+    "body" TEXT,
+    "date" TEXT,
+    "source_id" INTEGER,
+    FOREIGN KEY (source_id) REFERENCES source(id)
+);
